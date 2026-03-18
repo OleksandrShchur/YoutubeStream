69b8f21339a4b0ade925397b7978127316126e02
Oleksandr Shchur <osur12709@gmail.com>
2026-03-18 18:54:36 +0200

    lower video quality

--- start-stream.sh
+++ start-stream.sh
@@ -12,10 +12,10 @@ if [ -z "$STREAM_KEY" ]; then
   exit 1
 fi
 
-ffmpeg -re -stream_loop -1 -i /app/video.mp4 \
-       -c:v libx264 -preset veryfast -tune zerolatency \
-       -b:v 1500k -maxrate 2000k -bufsize 4000k \
-       -vf 'scale=1920:1080:force_original_aspect_ratio=decrease,pad=1920:1080:(ow-iw)/2:(oh-ih)/2' \
-       -pix_fmt yuv420p -r 10 -g 60 -keyint_min 60 \
+ffmpeg -re -stream_loop -1 -r 10 -i /app/video.mp4 \
+       -c:v libx264 -preset ultrafast -tune zerolatency \
+       -b:v 800k -maxrate 1000k -bufsize 2000k \
+       -vf 'scale=1280:720:force_original_aspect_ratio=decrease,pad=1280:720:(ow-iw)/2:(oh-ih)/2' \
+       -pix_fmt yuv420p -r 10 -g 20 -keyint_min 20 \
        -c:a aac -b:a 128k -ar 44100 \
        -f flv "rtmp://a.rtmp.youtube.com/live2/${STREAM_KEY}"
