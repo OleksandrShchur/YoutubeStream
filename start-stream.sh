549c6c8f1c6f98448bef5edf748b6263524ea637
Oleksandr Shchur <osur12709@gmail.com>
2026-03-18 19:09:35 +0200

    Revert "pre encode video"
    This reverts commit 3f0fd83813dfe5a9e40b18af90a7298f9db8fd3a.

--- start-stream.sh
+++ start-stream.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 set -e
 
-# === KEEP-ALIVE: tiny HTTP server (prevents free-tier spin-down) ===
 python3 -m http.server ${PORT:-10000} > /dev/null 2>&1 &
-
 echo "Keep-alive server started on port ${PORT:-10000}"
 
 STREAM_KEY="${YOUTUBE_STREAM_KEY}"
@@ -12,10 +10,6 @@ if [ -z "$STREAM_KEY" ]; then
   exit 1
 fi
 
-ffmpeg -re -stream_loop -1 -r 10 -i /app/video.mp4 \
-       -c:v libx264 -preset ultrafast -tune zerolatency \
-       -b:v 800k -maxrate 1000k -bufsize 2000k \
-       -vf 'scale=1280:720:force_original_aspect_ratio=decrease,pad=1280:720:(ow-iw)/2:(oh-ih)/2' \
-       -pix_fmt yuv420p -r 10 -g 20 -keyint_min 20 \
-       -c:a aac -b:a 128k -ar 44100 \
+ffmpeg -re -stream_loop -1 -i /app/video.mp4 \
+       -c copy \
        -f flv "rtmp://a.rtmp.youtube.com/live2/${STREAM_KEY}"
